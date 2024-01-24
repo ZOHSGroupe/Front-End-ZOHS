d05ad18b0c7382b3aaaa290cc86861c69421521a
SadikHajar <sadik.hajar@etu.uae.ac.ma>
2024-01-24 17:09:51 +0100

    home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { VehiculeComponent } from './Page/vehicule/vehicule.component';
+import { HomeComponent } from './Page/home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/vehicules', pathMatch: 'full' }, // Redirect to /vehicules by default
   { path: 'vehicules', component: VehiculeComponent },
   // Add more routes as needed
+  { path: 'Home', component: HomeComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VehiculeComponent } from './Page/vehicule/vehicule.component';
+import { HomeComponent } from './Page/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VehiculeComponent
+    VehiculeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
